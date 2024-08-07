9f5d7d03d40d4f27f4fe66ebcfc33ebfb5e36954
Dana <dsofia0528@gmail.com>
2024-08-07 01:25:27 -0500

    Eliminar Categoria

--- src/main/java/com/company/inventory/services/CategoryServiceImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServiceImpl.java
@@ -158,6 +158,27 @@ public class CategoryServiceImpl implements ICategoryService {
 		
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}
+
+	@Override
+	@Transactional
+	public ResponseEntity<CategoryResponseRest> deleteById(Long id) {
+CategoryResponseRest response = new CategoryResponseRest();
+		
+		try {
+			
+			CategoryDao.deleteById(id);
+			response.setMetadata("Respuesta OK", "00", "Registro Eliminado");
+			
+		} catch(Exception e) {
+			
+			response.setMetadata("Respuesta NO Ok", "-1", "Error al eliminar");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			
+		}
+		
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
 	
 	
 
--- src/main/java/com/company/inventory/services/ICategoryService.java
+++ src/main/java/com/company/inventory/services/ICategoryService.java
@@ -15,6 +15,8 @@ public interface ICategoryService {
 	
 	public ResponseEntity<CategoryResponseRest> update(Category category, Long id);
 	
+	public ResponseEntity<CategoryResponseRest> deleteById(Long id);
+	
 	
 	
 	
